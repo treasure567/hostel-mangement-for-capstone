7082eda0b76bbc405e8a17bd27a502b85504c905
okereke helen <65390055+helen751@users.noreply.github.com>
2021-11-05 21:43:31 +0100

    done with the UI
    all the ui of the hostel management system website

---
+++ signup.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Register</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style2.css">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-7 col-lg-5">
+					<div class="login-wrap">
+		      	<div class="img" style="background-image: url(images/mat2.jpg);"></div>
+		      	<h3 class="text-center mb-4">Register</h3>
+						<form action="#" class="signup-form">
+		      		<div class="form-group mb-3">
+		      			<input type="text" class="form-control" placeholder="Your Reg No:">
+		      		</div>
+		      		<div class="form-group mb-3">
+		      			<input type="email" class="form-control" placeholder="Your Email:">
+		      		</div>
+					  <div class="form-group mb-3">
+						<input type="text" class="form-control" placeholder="Your Username:">
+					</div>
+	            <div class="form-group mb-3">
+	              <input id="password-field" type="password" class="form-control" placeholder="Password">
+	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+	            </div>
+	            <div class="form-group">
+	            	<button type="submit" class="form-control btn btn-primary submit px-3">Register</button>
+	            </div>
+	          </form>
+	          <p>I'm already a member! <a href="index.html">Sign In</a></p>
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
